705f1e3faaad5de59842933d7612804ec0d966c2
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-26 22:10:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
